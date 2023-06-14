be84ef9ddae8171c648e03f378be15b267ac1068
JoseManuelBarba <jose09511@gmail.com>
2023-06-14 16:28:46 +0200

    ciclo for y for in

---
+++ 02_control_flujo/03_for.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<int> elementos = [1, 2, 3, 4, 5, 6];
+  for (int i = 0; i <= elementos.length - 1; i++) {
+    print('index i:::: $i');
+    print(elementos[i]);
+  }
+}
---
+++ 02_control_flujo/04_for_in.dart
@@ -0,0 +1,26 @@
+void main() {
+  List<dynamic> listado = [
+    "Dart",
+    2,
+    "APIRest",
+    {"color": "verde"}
+  ];
+
+  // for (var i = 0; i < listado.length; i++) {
+  //   print(listado[i]);
+  // }
+
+  // listado.forEach((element) {
+  //   print(element);
+  // });
+
+  for (var element in listado) {
+    if (element is Map) {
+      if (element["color"] == "verde") {
+        print("Puede ser una rana");
+        break;
+      }
+    }
+    print(element);
+  }
+}
